94001e1212775ebf0c0a778b2c10778f1ec12986
michaelbennie <michaelbennie@ufl.edu>
2024-04-10 15:07:47 -0400

    Refactor script to convert data to multiple CSVs
    The existing Python script originally converted input data into SQL inserts. This change refactors the script to generate multiple distinct CSV files, each representing data pertaining to accidents, time, and weather respectively. Additionally, the output files have been added to the '.gitignore' file to avoid including them in the repository.

--- pythonScripts/convertDataIntoSQL.py
+++ pythonScripts/convertDataIntoSQL.py
@@ -42,45 +42,98 @@ def generateAirportInserts(csv_file_path='../data/iata-icao.csv',
         output_file.close()
 
 
-def generateAccidentInserts(csv_file_path='../data/US_Accidents_March23.csv',
-                            output_directory_path='../sqlScripts/AccidentInserts', maxRowsPerFile=80000):
-    readLineCount = 0
-    fileIndex = 0
-    output_file_path = f"{output_directory_path}/accident_inserts_{fileIndex}.sql"
-
+def generateAccidentCsv(csv_file_path='../data/US_Accidents_March23.csv',
+                        output_file_path='../csvOutputs/Accident.csv'):
     with open(csv_file_path, mode='r') as csv_file:
         csv_reader = csv.DictReader(csv_file)
 
         # 確保輸出目錄存在
+        output_directory_path = os.path.dirname(output_file_path)
         os.makedirs(output_directory_path, exist_ok=True)
 
-        output_file = open(output_file_path, mode='w')
+        # 打開輸出文件
+        with open(output_file_path, mode='w', newline='') as output_file:
+            csv_writer = csv.writer(output_file)
 
-        for i,row in enumerate(csv_reader):
-            ID = row['ID']
-            Source = row['Source']
-            Severity = row['Severity']
-            Distance = row['Distance(mi)']
-            #The description takes up too much data so we dont include it
-            #Description = row['Description'].replace("'", "''")
-            Start_Lat = row['Start_Lat']
-            Start_Lng = row['Start_Lng']
+            # 寫入列名稱
+            csv_writer.writerow(['ID', 'Source', 'Severity', 'DistanceAffected', 'LocStartLatitude', 'LocStartLongitude'])
 
-            insert_command = f"INSERT INTO Accident (ID, Source, Severity, DistanceAffected, LocStartLatitude, LocStartLongitude) VALUES ('{ID}', '{Source}', {Severity}, {Distance}, {Start_Lat}, {Start_Lng});\n"
+            for row in csv_reader:
+                ID = row['ID']
+                Source = row['Source']
+                Severity = row['Severity']
+                Distance = row['Distance(mi)']
+                Start_Lat = row['Start_Lat']
+                Start_Lng = row['Start_Lng']
 
-            output_file.write(insert_command)
-            readLineCount += 1
+                # 寫入數據行
+                csv_writer.writerow([ID, Source, Severity, Distance, Start_Lat, Start_Lng])
 
-            if readLineCount % maxRowsPerFile == 0 and readLineCount:
-                print("row", i)
-                output_file.close()
-                fileIndex += 1
-                output_file_path = f"{output_directory_path}/accident_inserts_{fileIndex}.sql"
-                output_file = open(output_file_path, mode='w')
 
-        output_file.close()
+def generateTimeCsv(csv_file_path='../data/US_Accidents_March23.csv',
+                        output_file_path='../csvOutputs/Time.csv'):
+    # 確保輸出目錄存在
+    output_directory_path = os.path.dirname(output_file_path)
+    os.makedirs(output_directory_path, exist_ok=True)
+
+    with open(csv_file_path, mode='r') as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        seen_ids = set()
+
+        with open(output_file_path, mode='w', newline='') as output_file:
+            fieldnames = ['AccidentID', 'StartTime', 'EndTime', 'TimeZone', 'TimePeriod']
+            csv_writer = csv.DictWriter(output_file, fieldnames=fieldnames)
+            csv_writer.writeheader()
+
+            for row in csv_reader:
+                accident_id = row['ID']
+                # 檢查此條目是否為唯一
+                if accident_id not in seen_ids:
+                    seen_ids.add(accident_id)
+                    csv_writer.writerow({
+                        'AccidentID': row['ID'],
+                        'StartTime': row['Start_Time'],
+                        'EndTime': row['End_Time'],
+                        'TimeZone': row['Timezone'],
+                        'TimePeriod': row['Sunrise_Sunset']
+                    })
+
+
+def generateWeatherCsv(csv_file_path='../data/US_Accidents_March23.csv',
+                        output_file_path='../csvOutputs/Weather.csv'):
+    # 確保輸出目錄存在
+    output_directory_path = os.path.dirname(output_file_path)
+    os.makedirs(output_directory_path, exist_ok=True)
+
+    with open(csv_file_path, mode='r') as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        seen_ids = set()
+
+        with open(output_file_path, mode='w', newline='') as output_file:
+            fieldnames = ['Temperature', 'WindSpeed', 'WeatherCondition', 'Visibility', 'WindDirection', 'Humidity', 'Pressure', 'WindChill','Precipitation']
+            csv_writer = csv.DictWriter(output_file, fieldnames=fieldnames)
+            csv_writer.writeheader()
+
+            for row in csv_reader:
+                accident_id = row['ID']
+                # 檢查此條目是否為唯一
+                if accident_id not in seen_ids:
+                    seen_ids.add(accident_id)
+                    csv_writer.writerow({
+                        'Temperature': row['Temperature(F)'],
+                        'WindSpeed': row['Wind_Speed(mph)'],
+                        'WeatherCondition': row['Weather_Condition'],
+                        'Visibility': row['Visibility(mi)'],
+                        'WindDirection': row['Wind_Direction'],
+                        'Humidity': row['Humidity(%)'],
+                        'Pressure': row['Pressure(in)'],
+                        'WindChill': row['Wind_Chill(F)'],
+                        'Precipitation': row['Precipitation(in)']
+                    })
 
 
 if __name__ == '__main__':
     #generateAirportInserts()
-    generateAccidentInserts()
+    #generateAccidentCsv()
+    #generateTimeCsv()
+    generateWeatherCsv()
