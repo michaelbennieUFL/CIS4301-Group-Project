1517542e105380097d73c8077899abdf9b40c065
michaelbennie <michaelbennie@ufl.edu>
2024-04-11 10:04:55 -0400

    Add constraints, modify latitude checks, enhance select statement
    This commit adds new primary key constraints to the RoadCondition and Weather tables. It also modifies the latitude constraint checks to be within valid geographical range for latitude (-90 to 90 degrees) in several tables (MakeTables.sql). Additionally, a more comprehensive select statement was implemented in selectAccidentData.sql to provide insights into average severity of accidents by year and month.

--- sqlScripts/setup/addConstraints.sql
+++ sqlScripts/setup/addConstraints.sql
@@ -4,3 +4,14 @@ ALTER TABLE MICHAELBENNIE.Time ADD CONSTRAINT fk_Time_Accident FOREIGN KEY (Acci
 
 --add constraints for location
 ALTER TABLE MICHAELBENNIE.LOCATION ADD CONSTRAINT pk_Location PRIMARY KEY (StartLatitude, StartLongitude);
+
+--add constraints for RoadCondition
+ALTER TABLE MICHAELBENNIE.ROADCONDITION ADD CONSTRAINT pk_RoadCondition PRIMARY KEY (LocStartLatitude, LocStartLongitude);
+
+
+--add constraint for Weather
+ALTER TABLE MICHAELBENNIE.WEATHER ADD CONSTRAINT pk_Weather PRIMARY KEY (AccidentID);
+
+
+--Add Indices for faster lookup
+CREATE unique INDEX idx_Time_AccidentID ON Time (AccidentID);
